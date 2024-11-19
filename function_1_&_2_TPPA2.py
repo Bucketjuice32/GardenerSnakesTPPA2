15a1f103ba77f20d002b37ec1baaa6b9bbcde3e5
philngyen <phillip.d.nguyen@wsu.edu>
2024-11-19 12:29:38 -0800

    Add files via upload

---
+++ function_1_&_2_TPPA2.py
@@ -0,0 +1,31 @@
+def patient_info():
+
+    first_name = input('Enter first name: ')
+
+    middle_name = input('Enter middle name; ')
+
+    last_name = input('Enter last name: ')
+
+    address = input('Enter address: ')
+
+    zip_code = input('Enter ZIP Code: ')
+
+    state = input('Enter state: ')
+
+    city = input('Enter City: ')
+
+    phone = input'Enter Phone Number: ')
+
+    em_name = input'Enter name of emergency contact: ')
+
+    em_phone = input('Enter name of emergency contact phone number: ')
+
+def procedures():
+
+    procedure_name = input('Enter procedure: ')
+
+    procedure_date = input('Enter procedure date: ')
+
+    procedure_doc = input('Enter Pracitioner Name: ')
+
+    procedure_charge = input('Enter Procedure Cost: ')
