00621b470a60cfd4a9505f2b9e21627a8ee15005
philngyen <phillip.d.nguyen@wsu.edu>
2024-11-26 13:55:26 -0800

    Add files via upload

---
+++ menu&main_functions.py
@@ -0,0 +1,27 @@
+
+# Global Constants
+
+lookup = 1
+add_info = 2
+change_info = 3
+delete_info = 4
+quit_program = 5
+
+    
+
+def main():
+
+    menu_choices()
+
+
+def menu_choices():
+
+    print('Menu', '\n--------------------------')
+    print('1. Lookup Info')
+    print('2. Add Info')
+    print('3. Change Info')
+    print('4. Delete Info')
+    print('5. Quit')
+
+
+main()
